2cacdbe55b65af35451bca2c44b93215aea5edc4
Craig Wendel <wendelcraig@gmail.com>
2017-11-21 10:36:50 -0600

    Add WFD to project section. Adjust media queries for responsive. Edit HTML links, add desktop image for WFD.

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,18 @@ $(document).ready(function () {
   })
 })
 
+// Add smooth scrolling to all links
+
 $(document).ready(function () {
-  $("a").on('click', function (event) {
-    if (this.hash !== "") {
+  $('a').on('click', function (event) {
+    if (this.hash !== '') {
       event.preventDefault()
+      // Store hash
       var hash = this.hash
+
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 1500, function () {
+      }, 1200, function () {
         window.location.hash = hash
       })
     }
